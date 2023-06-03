7a79274ce25ed98d24a0ec5f343b434810036ebf
merinj20 <merinj20.can@gmail.com>
2023-06-03 10:34:50 -0400

    jobcategories test case changes

--- src/test/java/ca/qaguru/oranghrmbatch27/pages/JobCategoriesPage.java
+++ src/test/java/ca/qaguru/oranghrmbatch27/pages/JobCategoriesPage.java
@@ -15,14 +15,13 @@ public class JobCategoriesPage extends PageBase {
     private final String addJobCategoryBtn = "//button[text()=' Add ']";
     private final String categoryNameTxtField = "//label[text()='Name']/following::input";
     private final String categorySaveBtn = "//button[text()=' Save ']";
-
     private final String categoryCancelBtn = "//button[text()=' Cancel ']";
-
     private final String categoryExistMsg = "//div[@class='oxd-form-row']/div";
     private final String categoryHeader = "//h6[text()='Job Categories']";
     private final String addJobCategoryHeader = "//h6[text()='Add Job Category']";
     private final String listOfJobCategories = ".oxd-table-body";
     private final String categories = "//div[@class='oxd-table-body']/div[@class='oxd-table-card']";
+
     @FindBy(xpath = categories)
     private List<WebElement> listCategory;
 
@@ -33,24 +32,25 @@ public class JobCategoriesPage extends PageBase {
 
     }
 
-    public void header() {
-        String header = getText(By.xpath(categoryHeader));
-        Assert.assertEquals(header, "Job Categories");
+    public String header() {
+        isElementVisible(By.xpath(categoryHeader));
+        return getText(By.xpath(categoryHeader));
     }
 
-    public void addJobCategoryheader() {
+    public String addJobCategoryheader() {
         click(By.xpath(addJobCategoryBtn));
-        String header = getText(By.xpath(addJobCategoryHeader));
-        Assert.assertEquals(header, "Add Job Category");
+        isElementVisible(By.xpath(addJobCategoryHeader));
+        return getText(By.xpath(addJobCategoryHeader));
     }
 
-    public void isMandatory(String categoryName) {
+    public boolean isMandatory() {
         click(By.xpath(addJobCategoryBtn));
-        setText(By.xpath(categoryNameTxtField), categoryName);
+        setText(By.xpath(categoryNameTxtField), "");
         click(By.xpath(categorySaveBtn));
         if (getText(By.xpath(categoryExistMsg)).contains("Required")) {
-            System.out.println("Mandatory field enabled");
+            return true;
         }
+        return false;
     }
 
     public void saveNewJobCategory(String categoryName) {
@@ -61,23 +61,22 @@ public class JobCategoriesPage extends PageBase {
         } else {
             click(By.xpath(categorySaveBtn));
         }
-        isElementVisible(By.cssSelector(listOfJobCategories));
-        Boolean textCategorypresent = false;
-        for (WebElement category : listCategory) {
-            String categoryText = category.getText();
-            //System.out.println(categoryText);
-            if (categoryName.equalsIgnoreCase(categoryText)) {
-                textCategorypresent = true;
-            }
-        }
-        Assert.assertTrue(textCategorypresent);
-        System.out.println("\n");
-        System.out.println(categoryName + " is added successfully");
-        System.out.println("\n");
     }
 
-
+    public boolean isJobCategoryAdded(String categoryName) {
+        isElementVisible(By.cssSelector(listOfJobCategories));
+        for (WebElement category : listCategory) {
+            String categoryText = category.getText();
+            if (categoryName.equalsIgnoreCase(categoryText)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
+
+
+
 
 
 
--- src/test/java/ca/qaguru/oranghrmbatch27/tests/JobCategoryTests.java
+++ src/test/java/ca/qaguru/oranghrmbatch27/tests/JobCategoryTests.java
@@ -5,48 +5,57 @@ import ca.qaguru.oranghrmbatch27.pages.HeaderPage;
 import ca.qaguru.oranghrmbatch27.pages.JobCategoriesPage;
 import ca.qaguru.oranghrmbatch27.pages.LoginPage;
 import ca.qaguru.oranghrmbatch27.pages.MenuOptions;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class JobCategoryTests extends TestBase {
 
 
+    public static final String FINANCE_ANALYST = "Finance";
+    public static final String USERNAME = "Admin";
+    public static final String PASSWORD = "admin123";
+
     @Test
     public void headerJobCategory() {
         LoginPage loginPage = new LoginPage(driver);
-        loginPage.login("Admin", "admin123", true, null);
+        loginPage.login(USERNAME, PASSWORD, true, null);
         HeaderPage headerPage = new HeaderPage(driver);
         headerPage.selectMenu(MenuOptions.JOB_CATEGORIES);
         JobCategoriesPage jobCategoriesPage = new JobCategoriesPage(driver);
-        jobCategoriesPage.header();
+        Assert.assertEquals(jobCategoriesPage.header(), "Job Categories");
     }
 
     @Test
     public void headerAddJobCategory() {
         LoginPage loginPage = new LoginPage(driver);
-        loginPage.login("Admin", "admin123", true, null);
+        loginPage.login(USERNAME, PASSWORD, true, null);
         HeaderPage headerPage = new HeaderPage(driver);
         headerPage.selectMenu(MenuOptions.JOB_CATEGORIES);
         JobCategoriesPage jobCategoriesPage = new JobCategoriesPage(driver);
-        jobCategoriesPage.addJobCategoryheader();
+        Assert.assertEquals(jobCategoriesPage.addJobCategoryheader(), "Add Job Category");
+
 
     }
+
     @Test
     public void mandatoryEnabled() {
         LoginPage loginPage = new LoginPage(driver);
-        loginPage.login("Admin", "admin123", true, null);
+        loginPage.login(USERNAME, PASSWORD, true, null);
         HeaderPage headerPage = new HeaderPage(driver);
         headerPage.selectMenu(MenuOptions.JOB_CATEGORIES);
         JobCategoriesPage jobCategoriesPage = new JobCategoriesPage(driver);
-        jobCategoriesPage.isMandatory("");
+        Assert.assertTrue(jobCategoriesPage.isMandatory());
     }
+
     @Test
     public void addNewCategory() {
         LoginPage loginPage = new LoginPage(driver);
-        loginPage.login("Admin", "admin123", true, null);
+        loginPage.login(USERNAME, PASSWORD, true, null);
         HeaderPage headerPage = new HeaderPage(driver);
         headerPage.selectMenu(MenuOptions.JOB_CATEGORIES);
         JobCategoriesPage jobCategoriesPage = new JobCategoriesPage(driver);
-        jobCategoriesPage.saveNewJobCategory("Finance Analyst");
+        jobCategoriesPage.saveNewJobCategory(FINANCE_ANALYST);
+        Assert.assertTrue(jobCategoriesPage.isJobCategoryAdded(FINANCE_ANALYST));
     }
 
 }
